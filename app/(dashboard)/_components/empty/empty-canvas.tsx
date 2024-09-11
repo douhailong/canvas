2d54b9c4f321887647ac33ad169cd68c65261dc1
douhl <Dhl@0422>
2024-09-11 17:40:24 +0800

    test

--- app/(dashboard)/_components/empty/empty-canvas.tsx
+++ app/(dashboard)/_components/empty/empty-canvas.tsx
@@ -32,7 +32,7 @@ const EmptyCanvas = () => {
       {/* <Image src='/empty-canvas.svg' alt='empty' width={180} height={180} /> */}
       <h2 className='mt-6 text-2xl font-semibold'>Create your first canvas!</h2>
       <p className='mt-2 text-sm text-muted-foreground'>
-        Start by creating a cavas for your organization
+        Start by creating a cavas for your organization.
       </p>
       <Button className='mt-6' size='lg' onClick={onClick}>
         Create canvas
